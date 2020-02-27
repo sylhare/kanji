cf6cd687643283c59cec3e1b93c0ab74fc8bf933
sylhare <sylhare@outlook.com>
2020-02-27 15:50:08 -0500

    Add gulp tasks

---
+++ assets/js/gulp/gulpfile.js
@@ -0,0 +1,30 @@
+/*
+ * Gulp tasks
+ */
+const gulp = require('gulp');
+const concat = require('gulp-concat');
+let uglify = require('gulp-uglify-es').default;
+const imagemin = require('gulp-imagemin');
+
+
+gulp.task('js', function minijs() {
+  return gulp.src(['../main.js'])
+    .pipe(concat('main.min.js'))
+    .pipe(uglify())
+    .on('error', (err) => {
+      console.log(err.toString());
+    })
+    .pipe(gulp.dest("../"))
+});
+
+gulp.task("img", function imging() {
+  return gulp.src('../../img/**/*.{png,svg,jpg,webp,jpeg,gif}')
+    .pipe(imagemin())
+    .on('error', (err) => {
+      console.log(err.toString());
+    })
+    .pipe(gulp.dest('../../img/'))
+});
+
+
+gulp.task("default", gulp.series(gulp.parallel('js', 'img')));
