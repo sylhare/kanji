ab8cc078b5102fbdee1b2b4fad12c9892910a2d4
sylhare <sylhare@outlook.com>
2020-04-03 09:22:48 -0400

    Refactor graph

--- assets/js/graph/graph.js
+++ assets/js/graph/graph.js
@@ -1,4 +1,8 @@
 var width = 900, height = 680;
+const simulationDurationInMs = 20000; // 20 seconds
+
+let startTime = Date.now();
+let endTime = startTime + simulationDurationInMs;
 
 var svg = d3.select("#graph-kanji")
   .attr("width", width)
@@ -8,22 +12,20 @@ var color = d3.scaleOrdinal(d3.schemeCategory20);
 
 
 var simulation = d3.forceSimulation()
-  .force("link", d3
-    .forceLink()
+  .force("link", d3.forceLink() // Acts on the link of the graph
     .id(function(d) { return d.id; })
-    .distance(frequencySize(14)))
-  .force("charge", d3
-    .forceManyBody()
+    .distance(14))
+  .force("charge", d3.forceManyBody()
     .strength(10)
     .distanceMin(60)
     .distanceMax(600))
-  .force("collide",d3.forceCollide()
+  .force("collide",d3.forceCollide() // Acts on the node of the graph
     .radius(frequencySize(14))
     .iterations(32))
   .force("center", d3.forceCenter(width / 2, height / 2))
   .force("x", d3.forceX())
   .force("y", d3.forceY())
-  .velocityDecay(0.4)
+  //.velocityDecay(0.4)
   .alphaTarget(0.1);
 
 d3.json(jsonUrl, function(error, graph) {
@@ -40,28 +42,33 @@ d3.json(jsonUrl, function(error, graph) {
     .attr("class", "nodes")
     .selectAll("g")
     .data(graph.nodes)
-    .enter().append("g");
+    .enter().append("g")
+    .attr("class", "node");
 
-  // Circles
-  node.append("circle")
+  var circles = node.append("circle")
     .attr("class", function(d) { return d.group } )
-    //.attr("fill", function(d) { return color(d.group); })
-    //.attr("r", 5)
     .attr("r", frequencySize(12))
     .call(d3.drag()
       .on("start", dragstarted)
       .on("drag", dragged)
       .on("end", dragended));
 
-  // Labels
-  node.append("text")
+  var texts = node.append("text")
+    .attr("class", "kanji")
 //    .filter(function(d) {return d.frequency > 100 || d.connections;})
     .attr('x', -8)
     .attr('y', 6)
     .text(function(d) {return d.name;});
 
-  node.append("title")
-    .text(function(d) { return d.name; });
+  var title = node.append("title")
+    .text(function(d) { return d.name.concat(" - ", d.reading, " - ", d.meaning); });
+
+  var labels = node.append("text")
+    .attr("class", "label")
+    .attr('x', -8)
+    .attr('y', 6)
+    .attr("dy", ".35em")
+    .text(function(d) { return d.reading.concat(" - ", d.meaning);});
 
   simulation
     .nodes(graph.nodes)
@@ -71,6 +78,8 @@ d3.json(jsonUrl, function(error, graph) {
     .links(graph.links);
 
   function ticked() {
+    if (Date.now() < endTime) {
+      /*update the simulation*/
     link
       .attr("x1", function(d) { return d.source.x; })
       .attr("y1", function(d) { return d.source.y; })
@@ -80,7 +89,15 @@ d3.json(jsonUrl, function(error, graph) {
     node
       .attr("transform", function(d) {
         return "translate(" + d.x + "," + d.y + ")";
-      })
+      });
+
+    labels;
+    circles;
+    texts;
+    title;
+    } else {
+      simulation.stop();
+    }
   }
 });
 
